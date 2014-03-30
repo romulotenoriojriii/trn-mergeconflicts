59710e53b151399e1baee5a683c332289508f46d
mikaelvg <mgulapa@odecee.com.au>
2014-03-30 23:52:18 +0800

    Initial commit

---
+++ src/main/java/com/devkinetics/training/App.java
@@ -0,0 +1,14 @@
+package com.devkinetics.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {   
+        String myName = "Mikael Gulapa";
+        System.out.println( "my name is " + myName );
+    }
+}
