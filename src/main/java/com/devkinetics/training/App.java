79c8d2f713ae7091485aa9876580c3b4c3c86518
kenblizzard <kenneth.bolico@gmail.com>
2014-03-31 10:11:43 +0800

    2nd commit * Added more comments

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {   
-        String myName = "Kenneth Bolico";
-        System.out.println( "my name is " + myName );
+        String myName = "Aldwin Delgado"; //name of trainee
+        System.out.println( "my name is " + myName ); //this line will print the name 
     }
 }
