f21c2d620ad05027d857f87eca8d067c93e93a7e
John Mark <saysonjohnmark@gmail.com>
2014-03-31 16:50:34 +0800

    Macky

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
         String myName = "Mikael Gulapa";
-
+	System.out.println("ETO! "); //heheheheh
         System.out.println( "my name is " + myName );
         System.out.println( "Aldwin Paolo Delgado added a new line"); //Line ko :P
 	System.out.println( "Kenneth Bolico added a new line"); // (^,....,^)
