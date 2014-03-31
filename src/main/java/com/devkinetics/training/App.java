be1bc7609e11865a96d8b9b6850173558665da14
Lance Michael Samonte <lance.samonte.22@gmail.com>
2014-03-31 15:23:24 +0800

    First Commit by Lance Samonte

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String myName = "John Mark Sayson"; /* Change this to your own name */
+        String myName = "Lance Michael Samonte"; /* Change this to your own name */
         System.out.println( "my name is " + myName );
     }
 }
