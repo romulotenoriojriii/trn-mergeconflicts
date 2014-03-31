cbe0bb93ff0f96fefd01b32076c27b22529a3245
kenblizzard <kenneth.bolico@gmail.com>
2014-03-31 10:43:36 +0800

    Added step 4

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -4,11 +4,16 @@ package com.devkinetics.training;
  * Hello world!
  *
  */
+
+/**
+ * las commit : kenneth bolico
+ */
 public class App 
 {
     public static void main( String[] args )
     {   
-        String myName = "Aldwin Delgado"; //name of trainee
+        String myName = "Mikael Gulapa"; //name of trainer
         System.out.println( "my name is " + myName ); //this line will print the name 
+	System.out.println("Kenneth Bolico added a new line"); //add new line below
     }
 }
