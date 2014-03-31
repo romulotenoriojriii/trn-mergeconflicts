f5bb74eadf29d116082c31e1ae251af63afb3154
CIERON <cieron.panaguiton@gmail.com>
2014-03-31 15:55:05 +0800

    added cieron

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -15,8 +15,9 @@ public class App
         System.out.println( "Aldwin Paolo Delgado added a new line"); //Line ko :P
 		System.out.println( "Kenneth Bolico added a new line"); // (^,....,^)
 		System.out.println( "Bryan Condino added a new line"); // Epic
-		System.out.println( "Joy Esparcia added a new line"); //added new line 
-		System.out.println( "Czarina Lou Marave added a new line"); //added new line 
-		System.out.println( "Danica Sharon Narciso added a new line"); //added new line 
+		System.out.println( "Joy Esparcia added a new line"); //added new line
+		System.out.println( "Czarina Lou Marave added a new line"); //added new line
+		System.out.println( "Danica Sharon Narciso added a new line"); //added new line
+		System.out.println( "Cieron Panaguiton added a new line"); //added new line
     }
 }
