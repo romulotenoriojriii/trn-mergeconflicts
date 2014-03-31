24b35ff7cbdfb9cc52e07ecae55f1d75ab758469
kenblizzard <kenneth.bolico@gmail.com>
2014-03-31 13:31:53 +0800

    Commitment ni kenneth bolic

---
+++ desktop/trn-mergeconflicts/src/main/java/com/devkinetics/training/App.java
@@ -0,0 +1,15 @@
+package com.devkinetics.training;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {   
+        String myName = "Kenneth Bolico"; /* Change this to your own name */
+        System.out.println( "my name is " + myName );
+    }
+}
