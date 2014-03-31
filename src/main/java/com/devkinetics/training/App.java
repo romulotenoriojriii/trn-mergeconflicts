fb067d56372be6ffebc5e1ad226d1fd516553e67
Aldwin Paolo Delgado <aldwin_delgado017@yahoo.com>
2014-03-31 17:09:16 +0800

    Naglinis ng lines

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -14,12 +14,12 @@ public class App
 
         System.out.println("mas kita sa taas,  hehe"); // eris
         System.out.println("pull n lng kayo ulit.  hehe");  //eris
-	System.out.println("ETO AKIN -- MACKY! "); //heheheheh
-	System.out.println( "Jahren Respeto going for the conflict 2 added a new line"); //added new line
-	//<!----------------------------------------!>
-	
-	//Last na 'to bago mag out ;D
-	System.out.println("This is <3  "); //hahahahha
+		System.out.println("ETO AKIN -- MACKY! "); //heheheheh
+		System.out.println( "Jahren Respeto going for the conflict 2 added a new line"); //added new line
+		//<!----------------------------------------!>
+
+		//Last na 'to bago mag out ;D
+		System.out.println("This is <3  "); //hahahahha
 
         System.out.println( "my name is " + myName );
         System.out.println( "Aldwin Paolo Delgado added a new line"); //Line ko :P
@@ -29,32 +29,15 @@ public class App
 		System.out.println( "Czarina Lou Marave added a new line"); //added new line
 		System.out.println( "Danica Sharon Narciso added a new line"); //added new line
 		System.out.println( "Cieron Panaguiton added a new line"); //added new line
-		System.out.println("ABCD TIKI TIKI"); //Conflicting error
+		System.out.println( "ABCD TIKI TIKI"); //Conflicting error
 		System.out.println( "Erwin Praico added a new line"); //added new line
 		System.out.println( "Jahren Respeto added a new line"); //added new line
-        System.out.println( "Kenneth Bolico added a new line"); // (^,....,^)
-		System.out.println( "Bryan Condino added a new line"); // Epic
-		System.out.println( "Joy Esparcia added a new line"); //added new line
-		System.out.println( "Czarina Lou Marave added a new line"); //added new line
-		System.out.println( "Danica Sharon Narciso added a new line"); //added new line
-		System.out.println( "Cieron Panaguiton added a new line"); //added new line
-		System.out.println( "Erwin Praico added a new line"); //added new line
-		System.out.println( "Jahren Respeto added a new line"); //added new line
-		System.out.println( "Kenneth Bolico added a new line"); // (^,....,^)
-		System.out.println( "Bryan Condino added a new line"); // Epic
-		System.out.println( "Joy Esparcia added a new line"); //added new line
-		System.out.println( "Czarina Lou Marave added a new line"); //added new line
-		System.out.println( "Danica Sharon Narciso added a new line"); //added new line
-		System.out.println( "Cieron Panaguiton added a new line"); //added new line
-		System.out.println( "Erwin Praico added a new line"); //added new line
-		System.out.println( "Jahren Respeto added a new line"); //added new line
-		System.out.println( "John Mark Sayson added a new line"); //This is me
-        System.out.println( "John Eris Villanueva added a new line"); //added new line
+        System.out.println( "John Mark Sayson added a new line"); //This is me
+		System.out.println( "John Eris Villanueva added a new line"); //added new line
 		System.out.println( "Going for Step 4-Jahren"); //added new line
 		System.out.println( "Lance Michael Samonte added a new line"); //added new line
 		System.out.println( "Jahren Respeto going for the conflict added a new line"); //added new line
-
-                // ang gulo na po
+		// ang gulo na po
 
     }
 }
