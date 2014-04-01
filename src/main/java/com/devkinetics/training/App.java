2dad9a5f2bb9071b85fad97feb8a6c3a70380cd4
jko_respeto <jko_respeto@yahoo.com>
2014-03-31 17:48:59 -0700

    Step 4:Respeto

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -24,5 +24,7 @@ public class App
 	System.out.println( "John Mark Sayson added a new line"); //This is me
         // start conflicts xD
         System.out.println( "John Eris Villanueva added a new line"); //added new line
+
+		System.out.println( "Going for Step 4-Jahren"); //added new line
     }
 }
