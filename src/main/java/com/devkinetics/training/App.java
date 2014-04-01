e1bf168020e8b403510aa5a76649312c63a7f6df
johneris <johnerisvillanueva@gmail.com>
2014-03-31 17:14:20 -0700

    start conflicts

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -13,13 +13,17 @@ public class App
 
         System.out.println( "my name is " + myName );
         System.out.println( "Aldwin Paolo Delgado added a new line"); //Line ko :P
-		System.out.println( "Kenneth Bolico added a new line"); // (^,....,^)
-		System.out.println( "Bryan Condino added a new line"); // Epic
-		System.out.println( "Joy Esparcia added a new line"); //added new line
-		System.out.println( "Czarina Lou Marave added a new line"); //added new line
-		System.out.println( "Danica Sharon Narciso added a new line"); //added new line
-		System.out.println( "Cieron Panaguiton added a new line"); //added new line
-		System.out.println( "Erwin Praico added a new line"); //added new line
-		System.out.println( "Jahren Respeto added a new line"); //added new line
+        System.out.println( "Kenneth Bolico added a new line"); // (^,....,^)
+	System.out.println( "Bryan Condino added a new line"); // Epic
+	System.out.println( "Joy Esparcia added a new line"); //added new line
+	System.out.println( "Czarina Lou Marave added a new line"); //added new line
+	System.out.println( "Danica Sharon Narciso added a new line"); //added new line
+	System.out.println( "Cieron Panaguiton added a new line"); //added new line
+	System.out.println( "Erwin Praico added a new line"); //added new line
+	System.out.println( "Jahren Respeto added a new line"); //added new line
+        
+        // start conflicts xD
+        System.out.println( "John Eris Villanueva added a new line"); //added new line
+        
     }
 }
