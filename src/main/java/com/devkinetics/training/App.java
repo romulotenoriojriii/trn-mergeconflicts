02755e00d5ff9689b873637ab798d901074d791d
vjvallejera <vj.vallejera@gmail.com>
2014-07-02 09:48:59 +0800

    Learn first

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -4,6 +4,9 @@
     {
 
         String myName = "Victor";
+
+  System.out.println("victor" + myName + "Gentleman");
+  System.out.println("Learning programming added new line");
   System.out.println("Jiekokwago added new line"); //4/4/2014
     }
 }
