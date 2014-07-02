fc12a08816016b9b3bda6fe03f80733da1c9e3cc
mikaelvg <mikaelvg@gmail.com>
2014-07-02 09:28:36 +0800

    conflicter

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -3,9 +3,9 @@ public class App
     public static void main( String[] args )
     {
 
-        String myName = "Victor";
+        String myName = "Neri";
 
-        System.out.println("Victor" + myName);
+        System.out.println("Victor" + myName + " Artistahin ");
         System.out.println("Jiekokwago added new line"); //4/4/2014
     }
 }
