1831a488b0a39246dfd55351a7877775a60a773d
vjvallejera <vj.vallejera@gmail.com>
2014-07-02 08:51:39 +0800

    I love it

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -1,16 +1,9 @@
-package com.devkinetics.training;
-
-/**
- * Hello world!
- *
- */
-
 public class App
 {
     public static void main( String[] args )
     {
 
-     String myName = "I am superman";
-		System.out.println("spm added new line"); //4/4/201   
+        String myName = "Victor";
+  System.out.println("Superman ako added new line"); //4/4/2014
     }
 }
