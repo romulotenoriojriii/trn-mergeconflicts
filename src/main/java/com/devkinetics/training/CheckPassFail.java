42baa977f33323b27b3e33010ffdb2fd3bf2838d
vjvallejera <vj.vallejera@gmail.com>
2014-07-02 16:28:18 +0800

    First java training

---
+++ src/main/java/com/devkinetics/training/CheckPassFail.java
@@ -0,0 +1,15 @@
+package com.devkinetics.training.victorjohnvallejera.javabasic1;
+
+public class CheckPassFail { 
+    public static void main(String[] args) {
+        int Mark = 50 ; 
+        System.out.println("The Mark is + Mark");
+        if (Mark == 50) 
+        System.out.println("Past");
+    else 
+        System.out.println("Fail");
+        
+    }
+}
+  
+       
