6b0147f411c5dd021fe99d240cd1509ea3856e8d
vjvallejera <vj.vallejera@gmail.com>
2014-07-02 17:42:22 +0800

    First java training

---
+++ src/main/java/com/devkinetics/training/CheckOddEven.java
@@ -0,0 +1,14 @@
+package com.devkinetics.training.victorjohnvallejera.javabasic1;
+
+public class CheckOddEven { 
+    public static void main(String[] args) {
+    int number = 49;
+    System.out.println("The number is + Odd");
+       if (number % 2 == 0)
+       
+           System.out.println("Odd");
+    else
+           System.out.println("Even");
+    }
+
+}
