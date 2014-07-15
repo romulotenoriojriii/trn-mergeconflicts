40d101db1de33a60afcfd921a2fc688d7f5d90c3
EJ Biglang-awa <ej_biglangawa@yahoo.com>
2014-07-15 14:23:55 +0800

    TRANING NUMBER1

---
+++ trn-mergeconflicts/src/main/java/com/devkinetics/training/App.java
@@ -0,0 +1,12 @@
+ public class App
+{
+    public static void main( String[] args )
+    {
+
+        String myName = "Emmanuel Joseph";
+
+<<<<<<< HEAD
+  System.out.println("My name is:" + myName + "AKA The Destructor");
+>>>>>>> upstream/master
+    }
+}
