a078219b07b7328dd3548998f1f9cca5779af2ae
mikaelvg <mikaelvg@gmail.com>
2014-07-15 14:37:39 +0800

    fixed error

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -1,17 +1,9 @@
- public class App
-{
-    public static void main( String[] args )
-    {
 
-        String myName = "Neri";
+public class App {
 
-<<<<<<< HEAD
-  System.out.println("victor" + myName + "Gentleman");
-  System.out.println("Learning programming added new line");
-  System.out.println("Jiekokwago added new line"); //4/4/2014
-=======
-        System.out.println("Victor" + myName + " Artistahin ");
-        System.out.println("Jiekokwago added new line"); //4/4/2014
->>>>>>> upstream/master
+    public static void main(String[] args) {
+
+        String myName = "Kim";
+        System.out.println("My name is " + myName);
     }
 }
