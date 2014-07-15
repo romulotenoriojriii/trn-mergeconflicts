4e2ec83c07ecd42096a517f4e45da70c8d0f8e93
mikaelvg <mikaelvg@gmail.com>
2014-07-15 14:48:18 +0800

    conflict

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -5,7 +5,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        String myName = "Emmanuel Joseph N. Biglang-awa";
+        String myName = "I create a conflict";
         System.out.println("My name is " + myName);
     }
 }
