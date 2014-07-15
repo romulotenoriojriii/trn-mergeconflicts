4a36d4499d297e556f0ae86b2d2727ee60d62bab
mikaelvg <mikaelvg@gmail.com>
2014-07-15 14:55:44 +0800

    conflict

--- src/main/java/com/devkinetics/training/App.java
+++ src/main/java/com/devkinetics/training/App.java
@@ -5,7 +5,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        String myName = "I create a conflict";
+        String myName = "I created a conflict again";
         System.out.println("My name is " + myName);
     }
 }
